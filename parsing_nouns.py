449e1a4a5b06d67d63a965bd4056238b39d38599
Gisela Moreira Ortt <giortt@amazon.com>
2022-01-26 11:27:40 -0300

    fixed bug in the formating of the card, fixed the function createThirdField

--- parsing_nouns.py
+++ parsing_nouns.py
@@ -18,6 +18,7 @@ def getWordAndPlural( field ):
     return words[1], words[4]
 
 
+#TODO: improve the method to find who is plural
 def checkIfWordsAreSimilar( first, second ):
     if( SequenceMatcher( None, first, second ).ratio() > 0.5 ):
         return True
@@ -37,25 +38,41 @@ def isFormattingCompliant( field ):
         return False
 
 
-#TODO: bug fix: some words dont have plural
+def createThirdField( secondField ):
+    splited = secondField.strip(" \n").split(" ")
+    thirdField = splited[-1]
+    secondField = ' '.join( splited[0:-1] )
+
+    return secondField, thirdField
+
+
+def joinFields( firstField, secondField, thirdField ):
+
+    return ";".join([firstField.strip(" "), secondField.strip(" "), thirdField.strip(" ")])+"\n"
+
+
+#TODO: Refactor
 if __name__ == "__main__":
     input_file = open(sys.argv[1],'r')
-    parsed_file = open("parsedNouns.txt", 'w+')
-    excluded = open("excluded.txt", "w+")
+    parsed_file = open("parsedNouns.txt", 'w')
+    excluded = open("excluded.txt", "w")
 
     for line in input_file.readlines():
         cleanLine = cleanCard(line)
-        secondField = separateFieldsWithTab(cleanLine)[1]
-        if( isFormattingCompliant( secondField ) == False ):
-            continue
-        if( fieldIncludePlural( secondField ) ):
-            singular, plural = getWordAndPlural( secondField )
-            if( checkIfWordsAreSimilar(singular, plural) ):
-                parsed_file.write(cleanLine)
+        firstField, secondField = separateFieldsWithTab(cleanLine)
+        secondField, thirdField = createThirdField( secondField )
+        answer = joinFields( firstField, secondField, thirdField )
+        if( isFormattingCompliant( secondField )  ):
+            if( fieldIncludePlural( secondField ) ):
+                word, plural = getWordAndPlural( secondField )
+                if( checkIfWordsAreSimilar( word, plural ) ):
+                    parsed_file.write(answer)
+                else:
+                    excluded.write(answer)
             else:
-                excluded.write(cleanLine)
+                parsed_file.write(answer)
         else:
-            parsed_file.write(cleanLine)
+            excluded.write(answer)
 
     parsed_file.close()
     input_file.close()
