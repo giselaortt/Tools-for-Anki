05eeebb36076c7ea9ff4e1ea047291e611a16989
Gisela Moreira Ortt <giortt@amazon.com>
2022-02-06 15:33:46 -0300

    ïmplemented pdf converter to txt

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -4,49 +4,51 @@ import pytesseract
 import argparse
 import cv2
 import os
+import re
 
 
-def convertImageToGrayScale( path_to_image, output_path ):
-    image = cv2.imread(args["path_to_image"])
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(output_path, gray)
+#TODO: checar se xist
+def convertImageToGrayScale( path_to_image, output_name ):
+    image = cv2.imread(path_to_image, cv2.IMREAD_GRAYSCALE)
+    cv2.imwrite(output_name, image)
 
 
 def imageFromPdf( pdf_path, output_folder, pages_required = None ):
-    images = convert_from_path('example.pdf')
+    images = convert_from_path(pdf_path)
     for i in range(len(images)):
-        if pages_required not None and i in pages_required :
-            images[i].save(output_folder + str(i+1) +'.jpg', 'JPEG')
+        if( pages_required is None or i in pages_required ):
+            images[i].save(output_folder + "/" + str(i+1) +'.jpg', 'JPEG')
 
 
-#TODO: add preprocessing steps
-def txtFromImage( image_path, output_path ):
-    file_ptr=open(output_path)
-    text = pytesseract.image_to_string(Image.open(filename))
+def txtFromImage( image_path, output_name ):
+    file_ptr=open( removeExtentionFromString(output_name)+".txt", "w")
+    text = pytesseract.image_to_string(Image.open(image_path))
     file_ptr.write( text )
     file_ptr.close()
-    cv2.imshow("Image", image)
-    cv2.imshow("Output", gray)
-    cv2.waitKey(0)
+
+
+def removeExtentionFromString( text ):
+
+    return re.sub( "\..{3}", "", text )
 
 
 def applyFunctionThroughWholeFolder( input_path, output_path, function ):
     filenames = os.listdir(input_path)
     for file_name in filenames:
-        function(input_path+file_name, output_path+file_name)
+        function(input_path+"/"+file_name, output_path+"/"+file_name)
 
 
 #TODO: PDF's need a diferent aproach, they need to have a new folder per input file. otherwise the code will overwrite the pages.
-if __name__ == '__main__'():
-    path_to_pdf = "pdfs"
+if __name__ == '__main__':
+    path_to_pdf = "pdfs/Prufungstraining_Goethe-Zertifikat_C1.pdf"
     path_to_texts_folder = "extracted_txts"
     os.system("mkdir images")
     os.system("mkdir gray_images")
     path_to_images = "images"
-    path_to_gray = "gray"
+    path_to_gray = "gray_images"
 
     imageFromPdf( path_to_pdf, path_to_images )
     applyFunctionThroughWholeFolder( path_to_images, path_to_gray, convertImageToGrayScale )
-    applyFunctionThroughWholeFolder( path_to_gray, extracted_txts )
+    applyFunctionThroughWholeFolder( path_to_gray, path_to_texts_folder, txtFromImage )
 
-    os.system("rm -r images gray_images")
+    #os.system("rm -r images gray_images")
