36d60fda4e0608d551e673d162f44b316c375a0d
Gisela Moreira Ortt <giortt@amazon.com>
2022-02-23 13:19:43 -0300

    started a parsing function

--- eliminate_repetitions/generate_plurals.py
+++ eliminate_repetitions/generate_plurals.py
@@ -1,24 +1,63 @@
+# -*- coding: utf-8 -*-
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
+import os
+import re
+import sys
+sys.path.insert(1, "/Users/giortt/Desktop/readlang_to_anki/")
+from readlang_intgration.parsing import separateFields
+from unidecode import unidecode
 
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
-
-url = "https://www.verbformen.pt/declinacao/substantivos/?w=studium"
-html = urllib.request.urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, 'html.parser')
-
-# Retrieve all of the anchor tags
-tags = soup('p')
-for tag in tags:
-    #if("vStm rCntr" in tag):
-    print(tag)
 
 
+def getPluralsFromWebRequest():
+    ans = open("answer.txt", "a")
+    # Ignore SSL certificate errors
+    ctx = ssl.create_default_context()
+    basic_url = "https://www.verbformen.pt/declinacao/substantivos/?w="
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    file_ptr = open("most frequent nouns", "r")
+    lines = file_ptr.readlines()
+    words = []
+    for line in lines:
+        words.append( separateFields(line)[0] )
+    plurals=[]
+    for word in words[1:10]:
+        url = basic_url+word
+        html = urllib.request.urlopen(url, context=ctx).read()
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # Retrieve all of the anchor tags
+        tags = soup.find_all(class_="vStm rCntr")
+        for tag in tags:
+            print(tag.get_text().split("\n")[-1])
+            #ans.write(tag.get_text())
+
+        break
+
+    #print(unidecodeplurals)
+    #print(unidecode(plurals[0]))
+    ans.close()
+
+
+def cleanNumbers( card ):
+
+    return re.sub('[0-9]', '', card)
+
+
+def parsing( ):
+    fileptr = open("answer.txt", "r")
+    lines = fileptr.readlines()
+    for line in lines:
+        print(cleanNumbers(line))
+    fileptr.close()
+
+parsing()
+
+#getPluralsFromWebRequest()
 '''
 <p class="vStm rCntr">
 <b><i></i>Studium<i>s</i></b>
